7ecda52f0d8a5c08b817600092c90afa68c4a9b0
John Cordoba <johncordobagor@outlook.com>
2026-03-05 07:43:04 -0500

    Add new routes for profesionales and otros in server.js; update console log message for environment variables.

--- dist/server.js
+++ dist/server.js
@@ -15,20 +15,29 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const database_1 = require("./database");
 const plans_routes_1 = __importDefault(require("./routes/plans.routes"));
 const clinica_routes_1 = __importDefault(require("./routes/clinica.routes"));
+const profesional_routes_1 = __importDefault(require("./routes/profesional.routes"));
+const otros_routes_1 = __importDefault(require("./routes/otros.routes"))
+
 dotenv_1.default.config();
-console.log("Variable de entoron cargadas...");
+console.log("Variable de entorno cargadas...");
 const app = (0, express_1.default)();
 console.log("express cargado...");
+
 // Middlewares
 app.use((0, cors_1.default)());
 app.use(express_1.default.json()); //Para leer el json
+
 // Conexion MongoDB
 console.log("Conectando a mongoDB...");
 (0, database_1.connectDB)();
+
 //Routes
 app.use("/api/planes", plans_routes_1.default);
 app.use("/api/clinicas", clinica_routes_1.default);
+app.use("/api/profesionales", profesional_routes_1.default);
+app.use("/api/otros", otros_routes_1.default);
 app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "../uploads")));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
