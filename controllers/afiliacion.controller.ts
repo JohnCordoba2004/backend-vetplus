ccf5cf7508ed2bf84dd6dc91364fe7abe84f8a9b
John Cordoba <johncordobagor@outlook.com>
2026-03-30 13:49:18 -0500

    feat: Implementar envío de correos electrónicos al crear una afiliación
    - Se añadió la funcionalidad para enviar un correo electrónico al asesor con los detalles del nuevo cliente al crear una afiliación.
    - Se incorporó la dependencia `resend` para gestionar el envío de correos electrónicos.
    - Se calcularon y mostraron los valores de inscripción y mensualidad en el correo enviado.

--- controllers/afiliacion.controller.ts
+++ controllers/afiliacion.controller.ts
@@ -1,10 +1,44 @@
 import { Request, Response } from "express";
 import { Afiliacion } from "../models/Afiliacion";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const crearAfiliacion = async (req: Request, res: Response) => {
   try {
     const afiliacion = new Afiliacion(req.body);
     await afiliacion.save();
+
+    const ValorInscripcion = 70000;
+    const inscripcionMasCobertura = ValorInscripcion + req.body.valorMensual;
+
+    //Correo al afiliarse
+    await resend.emails.send({
+      from: "onboarding@resend.dev", //Domminio gratuito de resend
+      to: process.env.EMAIL_ASESOR!,
+      subject: "Nuevo cliente interesado en VetPlus",
+      html: `
+        <h2>Nuevo cliente interesado</h2>
+        <p><b>Nombre:</b> ${req.body.nombres} ${req.body.apellidos}</p>
+        <p><b>Documento de identidad:</b>${req.body.documento}</p>
+        <p><b>Ciudad:</b>${req.body.ciudad}</p>
+        <p><b>Telefono:</b> ${req.body.telefono}</p>
+        <p><b>Celular:</b> ${req.body.celular}</p>
+        <p><b>Direccion:</b> ${req.body.direccion}</p>
+        <p><b>Barrio/Localidad:</b> ${req.body.barrio}</p>
+        <p><b>Email:</b> ${req.body.email}</p>    
+        <p><b>Mascota:</b> ${req.body.nombreMascota} (${req.body.especie})</p>
+        <p><b>Fecha Nacimiento Mascota:</b> ${req.body.fechaNacimiento}</p>
+        <p><b>Color Mascota:</b> ${req.body.colorMascota}</p>
+        <p><b>Microchip:</b> ${req.body.microchip}</p>
+        <p><b>Valor inscripcion:</b>${ValorInscripcion}</p>
+        <p><b>Valor mensual del paquete:</b> ${req.body.valorMensual}</p>
+        <p><b>Valor total a pagar:</b> ${inscripcionMasCobertura}</p>
+        <p><b>Plan:</b> ${req.body.planSeleccionado}</p>
+        <p><b>Cobertura:</b>${req.body.cobertura}</p>
+      `,
+    });
+
     res.status(201).json({ ok: true, afiliacion });
   } catch (error) {
     console.error(error);
